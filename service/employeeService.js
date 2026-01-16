f6a2c817196e049247e4ca49781f764fdb75c426
devadatyapadhy-boop <devadatyapadhy@gmail.com>
2026-01-16 09:41:48 +0530

    response added

--- service/employeeService.js
+++ service/employeeService.js
@@ -1,12 +1,13 @@
 import Employee from "../model/employeeModel.js";
+import { employeeListResponse, employeeResponse } from "../response/employeeResponse.js";
 
 const getEmployees = async (req, res) => {
    try{
-        const emp=await Employee.find({$or:[{delted:false},
+        const emp=await Employee.find({$or:[{deleted:false},
           {deleted:{$exists:false}}
 
         ]});
-        res.status(200).json(emp);
+        res.status(200).json(employeeListResponse(emp));
       } catch(error){
         res.status(404).json({ message: error.message });
    }
@@ -16,7 +17,7 @@ const getOneEmployee = async (req, res) => {
    try{
 
     const emp=await Employee.findById(req.params.id);
-    res.status(200).json(emp);
+    res.status(200).json(employeeResponse(emp));
    } catch (error){
     res.status(404).json({ message: error.message });
    }
